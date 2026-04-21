ec2840dbfe82ca105c0ac4ebb0a14a1aafbb894a
mahodiii813-lgtm <mahodiii813@gmail.com>
2026-04-21 12:12:19 +0200

    ...
    ...

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 "use strict";
+console.log("Movie App starter...");
 
-
-console.log("Movie App - DAG 2 starter...");
-
-
-const movies=[
+const movies = [
   {
     title: "Inception",
     year: 2010,
-    rating: 8.8
+    rating: 8.8,
   },
   {
     title: "The Matrix",
     year: 1999,
-    rating: 8.7
+    rating: 8.7,
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    rating: 8.6,
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    rating: 9.0,
   },
 ];
-  
-console.log("Første film:", movies[0]);
-console.log("Anden film:", movies[1]);
+
+console.log("Alle film:", movies);
+
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
 
 for (const movie of movies) {
-  console.log("Titel:", movie.title);
+  const html = /* html */ `
+    <article class="movie-card">
+      <div class="movie-info">
+        <h3>${movie.title}</h3>
+        <p>År: ${movie.year}</p>
+        <p>Rating: ${movie.rating}</p>
+      </div>
+    </article>
+  `;
+
+  movieList.insertAdjacentHTML("beforeend", html);
 }
